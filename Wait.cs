a53067ed9c1fea8a284886a455e957e583614602
Boris Scheiman <bscheiman@gmail.com>
2013-11-29 11:30:58 -0600

    Adding Wait class

---
+++ Wait.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace iOS.Helpers {
+	public class Wait {
+		private readonly object LockObj = new object();
+		private volatile bool Continue;
+		private List<Action> Funcs { get; set; }
+		private int Sleep { get; set; }
+
+		internal Wait() {
+			Funcs = new List<Action>();
+		}
+
+		public void Abort() {
+			lock (LockObj) {
+				if (!Continue)
+					return;
+
+				Continue = false;
+			}
+		}
+
+		public Wait Commit(bool sameThread = false) {
+			Continue = true;
+
+			Action act = () => {
+				Thread.Sleep(Sleep);
+
+				lock (LockObj) {
+					if (!Continue)
+						return;
+
+					foreach (var f in Funcs)
+						f();
+				}
+			};
+
+			if (sameThread)
+				act();
+			else
+				new Thread(() => act()).Start();
+
+			return this;
+		}
+
+		public static Wait For(int ms) {
+			if (ms < 0)
+				throw new ArgumentOutOfRangeException();
+
+			return new Wait {
+				Sleep = ms
+			};
+		}
+
+		public Wait Then(Action f) {
+			if (f != null)
+				Funcs.Add(f);
+
+			return this;
+		}
+	}
+}
+
