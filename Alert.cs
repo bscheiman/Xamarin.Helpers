63d0d7a7e2604e3a15ae11a90bc422643d978bdf
Boris Scheiman <bscheiman@gmail.com>
2014-01-07 08:38:13 -0600

    Default values for AlertView

--- Alert.cs
+++ Alert.cs
@@ -1,11 +1,31 @@
 using System;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using System.Threading.Tasks;
 
 namespace iOS.Helpers {
 	public static class Alert {
 		static NSObject UIThread = new NSObject();
 		private static UIAlertView AlertView { get; set; }
+
+		public static Task<string> Input(string title, string message, string button = "Ok", UIKeyboardType type = UIKeyboardType.ASCIICapable, string defValue = "") {
+			var tcs = new TaskCompletionSource<string>();
+			AlertView = new UIAlertView(title, message, null, button, null);
+			AlertView.AlertViewStyle = UIAlertViewStyle.PlainTextInput;
+
+			var txt = AlertView.GetTextField(0);
+			txt.Text = defValue;
+			txt.KeyboardType = type;
+
+			AlertView.Clicked += (sender, e) => {
+				tcs.SetResult(AlertView.GetTextField(0).Text);
+			};
+
+			UIThread.InvokeOnMainThread(() => AlertView.Show());
+
+			return tcs.Task;
+		}
+
 
 		public static void Show(string title, string message, string button = "Ok", Action yesFunc = null) {
 			UIThread.InvokeOnMainThread(() => {
